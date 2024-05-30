eb3a191fba69930cd7f400345ed619eb5e6825fc
prakashSrivasta <prakash.21srivastava@gmail.com>
2024-05-30 14:25:01 +0530

    top-loading && infinite scroll

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -8,12 +8,19 @@ export class NewsItem extends Component {
       <>
         <div className="my-3">
           <div className="card">
+            <div style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0"
+            }}>
+
             <span
-              class="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
-              style={{ left: "90%", zIndex: "1" }}
+              className="badge rounded-pill bg-danger"              
             >
               {source}
             </span>
+            </div>
             <img
               src={
                 imgUrl
